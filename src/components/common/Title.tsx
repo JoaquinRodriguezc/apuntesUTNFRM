1c52b194a8a17c6eff6a21e657149e47da040b13
joaco <joaquin_rodriguez02@hotmail.com.ar>
2024-08-25 03:19:17 -0300

    center title

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Title() {
   return (
-    <div className="flex justify-center items-center flex-col h-1/6 gap-5">
+    <div className="flex justify-center items-center text-center flex-col h-1/6 gap-5">
       <h1 className="text-4xl font-bold"> APUNTES UTN MENDOZA </h1>
       <p className="text-xl font-semibold"></p>
     </div>
