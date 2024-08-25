d00f86ba7eb7cd61b1828fff273cd04c7732bb40
joaco <joaquin_rodriguez02@hotmail.com.ar>
2024-08-25 14:16:02 -0300

    add link to drive

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -4,6 +4,11 @@ function Title() {
   return (
     <div className="flex justify-center items-center text-center flex-col h-1/6 gap-5">
       <h1 className="text-2xl lg:text-4xl font-bold"> APUNTES UTN MENDOZA </h1>
+      <h2>
+        <a href={process.env.DRIVE_LINK}>
+          Clickeame para abrir drive directamente
+        </a>
+      </h2>
       <p className="text-xl font-semibold"></p>
     </div>
   );
